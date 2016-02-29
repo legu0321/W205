7d36cd7cb6ae3a60c02a665a256a93b449036b90
legu0321 <gule0321@gmail.com>
2016-02-28 23:24:30 -0800

    remove inline comments

--- exercise_1/investigations/hospital_and_patients.sql
+++ exercise_1/investigations/hospital_and_patients.sql
@@ -10,7 +10,7 @@ from temp_readmission_scores_regularized
 group by providerid;
 
 --hospital effective variablity vs Base Score 
-select corr(std_score,HCAHPS_Base_Score) -- -0.1
+select corr(std_score,HCAHPS_Base_Score)  
 from hospital_procedure_variability a
  join etl_surveys_response  b
   on a.providerid=b.Provider_Number;
@@ -18,48 +18,48 @@ from hospital_procedure_variability a
 
 --hospital readmission variablity vs Base Score 
 
-select corr(std_score,HCAHPS_Base_Score) -- -0.332
+select corr(std_score,HCAHPS_Base_Score) 
 from hospital_readmission_variability a
  join etl_surveys_response  b
   on a.providerid=b.Provider_Number;
  
 --hospital effective variablity vs Overall_Rating_of_Hospital_Achievement_Points Score 
-select corr(std_score,Overall_Rating_of_Hospital_Achievement_Points)    -- -0.19
+select corr(std_score,Overall_Rating_of_Hospital_Achievement_Points) 
 from hospital_procedure_variability a
  join etl_surveys_response  b
   on a.providerid=b.Provider_Number;
   
 --hospital readmission variablity vs Overall_Rating_of_Hospital_Achievement_Points 
-select corr(std_score,Overall_Rating_of_Hospital_Achievement_Points)  ---0.25
+select corr(std_score,Overall_Rating_of_Hospital_Achievement_Points)  
 from hospital_readmission_variability a
  join etl_surveys_response  b
   on a.providerid=b.Provider_Number;
  
 --hospital effective variablity vs Overall_Rating_of_Hospital_Dimension_Score Score   
- select corr(std_score,Overall_Rating_of_Hospital_Dimension_Score)  -- -0.19
+ select corr(std_score,Overall_Rating_of_Hospital_Dimension_Score)   
 from hospital_procedure_variability a
  join etl_surveys_response  b
   on a.providerid=b.Provider_Number;
 --hospital readmission variablity vs Overall_Rating_of_Hospital_Dimension_Score   
-select corr(std_score,Overall_Rating_of_Hospital_Dimension_Score)  ---0.24
+select corr(std_score,Overall_Rating_of_Hospital_Dimension_Score)   
 from hospital_readmission_variability a
  join etl_surveys_response  b
   on a.providerid=b.Provider_Number;
    
      
  --hospital score vs HCAHPS_Base_Score
- select corr(score_effective+score_readmission,HCAHPS_Base_Score) --0.019
+ select corr(score_effective+score_readmission,HCAHPS_Base_Score)  
 from prob1_combined a
  join etl_surveys_response  b
   on a.providerid=b.Provider_Number;
   --hospital score vs  Overall_Rating_of_Hospital_Achievement_Points
-select corr(score_effective+score_readmission,Overall_Rating_of_Hospital_Achievement_Points) --0.26
+select corr(score_effective+score_readmission,Overall_Rating_of_Hospital_Achievement_Points)  
 from prob1_combined a
  join etl_surveys_response b
   on a.providerid=b.Provider_Number;
 
 --hospital score vs Overall_Rating_of_Hospital_Achievement_Points
-select corr(score_effective+score_readmission,Overall_Rating_of_Hospital_Dimension_Score) --0.24
+select corr(score_effective+score_readmission,Overall_Rating_of_Hospital_Dimension_Score)  
 from prob1_combined a
  join etl_surveys_response b
   on a.providerid=b.Provider_Number;
